12cf712f46879bc828ce6bce82939b55ce3d8992
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-03-30 11:47:34 -0300

    first commit

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,205 @@
+import React, {useState} from 'react';
+import MyButtom from '../../components/MyButtom';
+import auth from '@react-native-firebase/auth';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TextInput,
+  Alert,
+} from 'react-native';
+import {CommonActions} from '@react-navigation/native';
+import { colors } from '../../assets/colors';
+import Loading from '../../components/Loading';
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+const SignIn = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function storeUserSession(email, pass) {
+    try {
+      await EncryptedStorage.setItem(
+        'user_session',
+        JSON.stringify({
+          email,
+          pass,
+        }),
+      );
+    } catch (error) {
+      console.error('SignIn, storeUserSession: ' + error);
+    }
+  }
+
+  const entrar = async () => {
+    if (email && password) {
+      try {
+        setLoading(true);
+        await auth().signInWithEmailAndPassword(email, password);
+        if (!auth().currentUser.emailVerified) {
+          Alert.alert('Erro', 'Você deve confirmar o seu email para prosseguir');
+          setLoading(false);
+          return;
+        }
+        await storeUserSession(email, password);
+        setLoading(false);
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'AppStack'}],
+          }),
+        );
+      } catch (e) {
+        setLoading(false);
+        console.error('SignIn, entrar: ' + e);
+        switch (e.code) {
+          case 'auth/user-not-found':
+            Alert.alert('Erro', 'Usuário não cadastrado.');
+            break;
+          case 'auth/wrong-password':
+            Alert.alert('Erro', 'Erro na senha.');
+            break;
+          case 'auth/invalid-email':
+            Alert.alert('Erro', 'Email inválido.');
+            break;
+          case 'auth/user-disabled':
+            Alert.alert('Erro', 'Usuário desabilitado.');
+            break;
+        }
+      }
+    }
+  };
+
+  const recuperarSenha = () => {
+    navigation.navigate('ForgotPassword');
+  };
+
+  const cadastrar = () => {
+    navigation.navigate('SignUp');
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.divSuperior}>
+          <Image
+            style={styles.image}
+            source={require('../../assets/images/logo.png')}
+            accessibilityLabel="logo do app"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            keyboardType="email-address"
+            returnKeyType="next"
+            onChangeText={t => setEmail(t)}
+          />
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="Senha"
+            keyboardType="default"
+            returnKeyType="go"
+            onChangeText={t => setPassword(t)}
+          />
+          <Text style={styles.textEsqueceuSenha} onPress={recuperarSenha}>
+            Esqueceu sua senha?
+          </Text>
+          <MyButtom text="ENTRAR" onClick={entrar} />
+        </View>
+        <View style={styles.divInferior}>
+          <View style={styles.divOuHr}>
+            <View style={styles.divHr} />
+            <Text style={styles.textOu}>OU</Text>
+            <View style={styles.divHr} />
+          </View>
+          <View style={styles.divCadastrarSe}>
+            <Text style={styles.textNormal}>Não tem uma conta?</Text>
+            <Text style={styles.textCadastrarSe} onPress={cadastrar}>
+              Cadastre-se
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+      {loading && <Loading />}
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  divSuperior: {
+    flex: 5,
+    alignItems: 'center',
+  },
+  divInferior: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    margin: 5,
+  },
+  input: {
+    width: '95%',
+    height: 50,
+    borderBottomColor: colors.gray,
+    borderBottomWidth: 2,
+    fontSize: 16,
+    paddingLeft: 2,
+    paddingBottom: 1,
+  },
+  textEsqueceuSenha: {
+    fontSize: 15,
+    color: colors.accentSecundary,
+    alignSelf: 'flex-end',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  divOuHr: {
+    width: '100%',
+    height: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  divHr: {
+    width: '30%',
+    height: 1,
+    borderBottomColor: colors.gray,
+    borderBottomWidth: 2,
+  },
+  textOu: {
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: 20,
+    color: colors.gray,
+  },
+  divCadastrarSe: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  textNormal: {
+    fontSize: 18,
+  },
+  textCadastrarSe: {
+    fontSize: 16,
+    color: colors.accentSecundary,
+    marginLeft: 5,
+  },
+});
---
+++ src/screens/SignUp/index.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth'; 
+import firestore from '@react-native-firebase/firestore';
+import { CommonActions } from '@react-navigation/native';
+
+import MyButtom from '../../components/MyButtom';
+import { Body, TextInput } from './styles';
+
+const SignUp = ({navigation}) => {
+  const [nome, setNome] = useState('');  
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
+  
+  const cadastrar = () => {
+    if (nome !== '' && email !== '' && pass !== '' && confirmPass !== '') {
+      if (pass === confirmPass) {
+        auth().createUserWithEmailAndPassword(email, pass)
+          .then(() => {
+            const userF = auth().currentUser;
+            const user = {};
+            user.nome = nome;
+            user.email = email;
+            userF.sendEmailVerification()
+              .then(() => {
+                Alert.alert('Informação', 'Um email de confirmação foi enviado para: .' + email);
+                navigation.dispatch(
+                  CommonActions.reset({
+                    index: 0,
+                    routes: [{name: 'SignIn'}]
+                  }),
+                );
+              })
+              .catch((e) => {
+                console.log(`SignUp, cadastrar: ${e}`);
+              })
+          })
+          .catch((e) => {
+            console.log(`SignUp, cadastrar: ${e}`);
+            switch(e.code){
+              case 'auth/email-already-in-use': 
+                Alert.alert('Erro', 'Email já esta em uso.'); 
+                break;
+              case 'auth/operation-not-allowed':
+                Alert.alert('Erro', 'Problema ao cadastrar usuário.'); 
+                break;
+              case 'auth/invalid-email':
+                Alert.alert('Erro', 'Email inválido.'); 
+                break;
+              case 'auth/weak-password':
+                Alert.alert('Erro', 'Senha fraca.'); 
+                break;
+            }
+          });
+      } else {
+        Alert.alert('Erro', 'Senhas devem ser iguais.');
+      }
+    }  else {
+      Alert.alert('Erro', 'Preencha todos os campos.');
+    }
+  };
+
+  return (
+    <Body>
+      <TextInput
+        placeholder="Nome completo"
+        keyboardType="default"
+        returnKeyType="next"
+        onChangeText={(t) => setNome(t)}
+        onEndEditing={() => this.emailTextInput.focus()}
+      />
+      <TextInput
+        ref={(ref) => {
+          this.emailTextInput = ref;
+        }}
+        placeholder="Email"
+        keyboardType="email-address"
+        returnKeyType="next"
+        onChangeText={(t) => setEmail(t)}
+        onEndEditing={() => this.passTextInput.focus()}
+      />
+      <TextInput
+        ref={(ref) => {
+          this.passTextInput = ref;
+        }}
+        secureTextEntry={true}
+        placeholder="Senha"
+        keyboardType="default"
+        returnKeyType="next"
+        onChangeText={(t) => setPass(t)}
+        onEndEditing={() => this.confirmPassTextInput.focus()}
+      />
+      <TextInput
+        ref={(ref) => {
+          this.confirmPassTextInput = ref;
+        }}
+        secureTextEntry={true}
+        placeholder="Confirmar senha"
+        keyboardType="default"
+        returnKeyType="go"
+        onChangeText={(t) => setConfirmPass(t)}
+        onEndEditing={() => cadastrar()}
+      />
+      <MyButtom text="cadastrar" onClick={cadastrar}/>
+    </Body>
+  );
+};
+
+export default SignUp;
