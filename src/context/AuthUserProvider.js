d53a58c1611ce464f1e5692317d047fb7511a26d
Aluno <aluno@example.com>
2023-04-27 17:46:46 -0300

    fix: removido codigos em desuso

--- src/context/AuthUserProvider.js
+++ src/context/AuthUserProvider.js
@@ -1,13 +1,10 @@
-import React, { createContext, useEffect, useState } from "react";
-import auth from '@react-native-firebase/auth';
+import React, { createContext } from "react";
 
 export const AuthUserContext = createContext({});
 
 export const AuthUserProvider = ({children}) => {
-  const [usuario, setUsuario] = useState();
-
   return (
-    <AuthUserContext.Provider value={{usuario, setUsuario}}>
+    <AuthUserContext.Provider value={{}}>
       {children}
     </AuthUserContext.Provider>
   );
