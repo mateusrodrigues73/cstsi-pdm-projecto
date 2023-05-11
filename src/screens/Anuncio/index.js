a7d7f35ed9bead61c6c4a1e6ae784bae7a08a925
Aluno <aluno@example.com>
2023-05-11 17:04:31 -0300

    feat: página de anúncio

---
+++ src/screens/Anuncio/index.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState, useContext } from 'react';
+import {Alert, ToastAndroid} from 'react-native';
+
+import Loading from '../../components/Loading';
+import MyButtom from '../../components/MyButtom';
+import {Container, TextInput} from './styles';
+import { UsuarioContext } from '../../context/UsuarioProvider';
+import { ProdutosContext } from '../../context/ProdutosProvider';
+
+const Anuncio = ({route, navigation}) => {
+  const {save, update} = useContext(ProdutosContext);
+  const {authUser} = useContext(UsuarioContext);
+  const [loading, setLoading] = useState(false);
+  const [id, setId] = useState(null);
+  const [modelo, setModelo] = useState('');
+  const [marca, setMarca] = useState('');
+  const [preco, setPreco] = useState('');
+
+  const salvar = async () => {
+    if (modelo && marca && preco) {
+      let produto = {};
+      produto.id = id;
+      produto.modelo = modelo;
+      produto.marca = marca;
+      produto.preco = preco;
+      produto.userId = authUser.uid;
+      setLoading(true);
+      if (id) {
+        if (await update(produto)) {
+          ToastAndroid.show(
+            'Show! Você alterou com sucesso.',
+            ToastAndroid.LONG,
+          );
+        } else {
+          ToastAndroid.show('Ops! Erro ao alterar.', ToastAndroid.LONG);
+        }
+      } else {
+        if (await save(produto)) {
+          ToastAndroid.show(
+            'Show! Você inluiu com sucesso.',
+            ToastAndroid.LONG,
+          );
+        } else {
+          ToastAndroid.show('Ops! Erro ao alterar.', ToastAndroid.LONG);
+        }
+      }
+      setLoading(false);
+      navigation.goBack();
+    } else {
+      Alert.alert('Atenção', 'Digite todos os campos.');
+    }
+  };
+
+  const excluir = () => {
+    alert('coming soon');
+  };
+
+  useEffect(() => {
+    if (route.params.produto) {
+      setModelo(route.params.produto.modelo);
+      setMarca(route.params.produto.marca);
+      setPreco(route.params.produto.preco);
+      setId(route.params.produto.id);
+    }
+  }, [route]);
+
+  return (
+    <Container>
+      <TextInput
+        placeholder="Modelo"
+        keyboardType="default"
+        returnKeyType="go"
+        onChangeText={t => setModelo(t)}
+        value={modelo}
+      />
+      <TextInput
+        placeholder="Marca"
+        keyboardType="default"
+        returnKeyType="go"
+        onChangeText={t => setMarca(t)}
+        value={marca}
+      />
+      <TextInput
+        placeholder="Preço"
+        keyboardType="default"
+        returnKeyType="go"
+        onChangeText={t => setPreco(t)}
+        value={preco}
+      />
+      <MyButtom text="Salvar" onClick={salvar} />
+      {id && <MyButtom text="Excluir" onClick={excluir} />}
+      {loading && <Loading />}
+    </Container>
+  );
+}
+
+export default Anuncio;
